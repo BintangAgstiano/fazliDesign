bcd40d9950b9774f998412546082ae62dc3f60d0
BintangAgstiano <bintangagustiano3@gmail.com>
2025-03-22 08:08:20 +0700

    update size

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 img.onload = () => resizeImage(img);
             }
         });
-    }, 3200);
+    }, 3100);
 
     function resizeImage(img) {
         const maxWidth = 1800; // Maksimal lebar gambar
